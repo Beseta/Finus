b34b264908bca49e80a899fcb52c62adb11d1b7e
Emilio Bernal <ebernalzubieta@gmail.com>
2022-12-09 10:23:30 -0600

    basic string parsing

--- finus/finus.ino
+++ finus/finus.ino
@@ -1,5 +1,7 @@
 #define LENGTH 512
 
+float noteFreqs[6] = {82.41, 110.00, 146.83, 196.00, 246.94, 329.63};
+char strings[6] = {'E', 'A', 'D', 'G', 'B', 'H'};
 byte rawData[LENGTH];
 int count;
 
@@ -13,6 +15,26 @@ int thresh = 0;
 float freq_per = 0;
 byte pd_state = 0;
 
+char getNote(float freq) {
+  int left = 0, right = 5;
+
+    while (left <= right) {
+        int mid = (left+right)/2;
+        if (noteFreqs[mid] == freq) {
+            return strings[mid];
+        } else if (noteFreqs[mid] < freq) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (abs(noteFreqs[left]-freq) > abs(noteFreqs[right]-freq)) {
+      return strings[right];
+    } else {
+      return strings[left];
+    }
+}
+
 void setup(){
   analogReference(EXTERNAL);   // Connect to 3.3V
   analogRead(A0);
@@ -22,17 +44,14 @@ void setup(){
 
 
 void loop(){
-  
   if (count < LENGTH) {
     count++;
     rawData[count] = analogRead(A0)>>2;
-  }
-  else {
+  } else {
     sum = 0;
     pd_state = 0;
     int period = 0;
-    for(i=0; i < len; i++)
-    {
+    for(i=0; i < len; i++) {
       // Autocorrelation
       sum_old = sum;
       sum = 0;
@@ -40,8 +59,7 @@ void loop(){
       // Serial.println(sum);
       
       // Peak Detect State Machine
-      if (pd_state == 2 && (sum-sum_old) <=0) 
-      {
+      if (pd_state == 2 && (sum-sum_old) <=0) {
         period = i;
         pd_state = 3;
       }
@@ -53,9 +71,10 @@ void loop(){
     }
     // for(i=0; i < len; i++) Serial.println(rawData[i]);
     // Frequency identified in Hz
-    if (thresh >100) {
+    if (thresh > 100) {
       freq_per = sample_freq/period;
-      Serial.println(freq_per);
+      // Serial.println(freq_per);
+      Serial.println(getNote(freq_per));
     }
     count = 0;
   }
